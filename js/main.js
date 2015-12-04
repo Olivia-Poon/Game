fe41d6f55f733d2d84a564e38fe235f80159378e
Graphic <graphic@SOD-SJH-516-10M.local>
2015-12-04 15:01:03 -0500

    added if statments and instructions

--- js/main.js
+++ js/main.js
@@ -10,32 +10,44 @@
 //repeat background
 var posX = 0;
 var speed = 3;
+var points = 0;
 var scroller = function() {
 	posX -= speed;
 	$('body').css('background-position', posX+'px 0');
-}
-//leopard animation
-var blink= function(){
+
 	$('.leopard').delay(random).animate({right: "+=3000"}, 2500).fadeOut(500,0).animate({right: "-=3000"}, 500).fadeIn(500,0)
+
+	$('.point').animate({right: "+=2500"}, 5500).fadeOut(500,0).animate({right: "+=2500"}, 5000).fadeIn(500,0);
+
+	if ($('.leopard').position().left == 0 && $('.panda').position().left == 0){
+		$(scroller).stop();
+
+		
+	}
+
+
+	if ($('.point').position().left == 0 && $('.panda').position().bottom == 250){
+		$('.pointh').append(points);
+		points++
+	}
 }
 
-//bampoo animation
-var bampoint= function(){
-	$('.point').animate({right: "+=2500"}, 5500).fadeOut(500,0).animate({right: "-=2500"}, 5000).fadeIn(500,0)
-}
 
 //generates random number between 1-5 seconds
 var random= Math.floor((Math.random() *5000) +1000); 
 
 $(document).ready(function() {
-	//setInterval(scroller, 5);
-	setInterval(blink, 5);
-	setInterval(bampoint,5);
+	$('body').append("How to Play: Collect the bamboo to earn points! If the leopard touches you gameover! Try to get as many points as you can").css("color","white")
+	setInterval(scroller, 5);
+
+
 	//makes panda jump on click
 	$(window).on('click', function(){
 		$('.panda').animate({bottom: "+=250"}, 500).animate({bottom: "-=250"}, 500)
 	})
+
 	
+
 });
 
 
