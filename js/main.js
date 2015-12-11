ff730436147bf74f1454cedd21de0029448e44e1
Graphic <graphic@SOD-SJH-516-10M.local>
2015-12-11 14:46:28 -0500

    if statements still don't work

--- js/main.js
+++ js/main.js
@@ -15,21 +15,10 @@ var scroller = function() {
 	posX -= speed;
 	$('body').css('background-position', posX+'px 0');
 
-	$('.leopard').delay(random).animate({right: "+=3000"}, 2500).fadeOut(500,0).animate({right: "-=3000"}, 500).fadeIn(500,0)
+	$('.leopard').delay(random).animate({right: "+=3000"}, 2500).fadeOut(500,0).animate({right: "-=3000"}, 500).fadeIn(500,0);
 
-	$('.point').animate({right: "+=2500"}, 5500).fadeOut(500,0).animate({right: "+=2500"}, 5000).fadeIn(500,0);
+	$('.point').delay(random).animate({right: "+=3000"}, 5500).fadeOut(500,0).animate({right: "-=3000"}, 5000).fadeIn(500,0);
 
-	if ($('.leopard').position().left == 0 && $('.panda').position().left == 0){
-		$(scroller).stop();
-
-		
-	}
-
-
-	if ($('.point').position().left == 0 && $('.panda').position().bottom == 250){
-		$('.pointh').append(points);
-		points++
-	}
 }
 
 
@@ -39,6 +28,19 @@ var random= Math.floor((Math.random() *5000) +1000);
 $(document).ready(function() {
 	$('body').append("How to Play: Collect the bamboo to earn points! If the leopard touches you gameover! Try to get as many points as you can").css("color","white")
 	setInterval(scroller, 5);
+
+		if ($('.leopard').position().left == 0 && ($('.panda').position().left == 0)){
+		$('body').stop();
+		$('.stop').show('fast');
+
+
+	}
+
+	if ($('.point').position().left == 0 && $('.panda').position().bottom == 250){
+		$('.pointh').append(points);
+		points++
+	}
+
 
 
 	//makes panda jump on click
